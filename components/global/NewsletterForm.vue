f6d84e36a30e028a5b8d3130cdf18d02af94aa74
Yusuf Parak <yusuf.parak@gmail.com>
2020-08-28 00:43:54 +0200

    Move newsletter to substack

--- components/global/NewsletterForm.vue
+++ components/global/NewsletterForm.vue
@@ -1,110 +1,85 @@
 <template>
-  <form
-    action="https://buttondown.email/api/emails/embed-subscribe/ycpso"
-    method="post"
-    target="popupwindow"
-    onsubmit="window.open('https://buttondown.email/ycpso', 'popupwindow')"
-    class="form">
-    <input
-      id="bd-email"
-      v-model="email"
-      type="email"
-      name="email"
-      class="input"
-      placeholder="Email">
-    <input type="hidden" value="1" name="embed"></input>
-    <input
-      type="submit"
-      value="Subscribe"
+  <div class="text-center">
+    <a
       class="button"
-      :disabled="isSubmitDisabled">
-    </input>
-  </form>
+      href="https://ycpso.substack.com"
+      target="_blank">
+      <MailIcon class="icon-email" /> Subscribe to email list
+    </a>
+  </div>
 </template>
 
 <script>
+import { MailIcon, RssIcon, TwitterIcon } from 'vue-feather-icons'
+
 export default {
   name: 'NewsletterForm',
-  data() {
-    return {
-      email: null
-    }
-  },
-  computed: {
-    isSubmitDisabled() {
-      if (!this.email || !this.validEmail(this.email)) {
-        return true
-      }
-
-      return false
-    }
-  },
-  methods: {
-    validEmail(email) {
-      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      return re.test(email)
-    }
+  components: {
+    MailIcon
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.form {
+.button {
   position: relative;
-  margin: 16px -12px 0 -12px;
+  display: inline-block;
+  width: 244px;
+  height: 32px;
+  text-align: center;
+  line-height: 31px;
+  margin: 16px 0 28px 0;
+  padding: 0 12px;
+  font-size: 11px;
+  color: var(--icon-color);
   background: var(--accent-color-1);
-  border-radius: 6px;
-  border: 1px solid var(--line-color);
-  box-shadow: var(--tooltip-shadow);
-  overflow: hidden;
-  .input {
-    background: var(--accent-color-1);
-    height: 32px;
-    line-height: 32px;
-    font-size: 14px;
-    width: 100%;
-    padding: 0 116px 0 12px;
-    color: var(--note-color);
-    font-weight: 600;
-    &::placeholder {
-      color: var(--text-color);
-      font-weight: 400;
-    }
-    @media (max-width: 767px) {
-      font-size: 13px;
-      padding: 0 96px 0 12px;
-    }
-  }
-  .button {
-    position: absolute;
-    top: 0;
-    right: 0;
-    height: 32px;
-    line-height: 0;
-    padding: 0 12px;
-    font-size: 11px;
-    border-left: 1px solid var(--newletter-submit-border);
-    color: var(--background-color);
-    background: var(--note-color);
-    font-weight: 720;
-    text-transform: uppercase;
-    letter-spacing: 2px;
-    border-top-right-radius: 6px;
-    border-bottom-right-radius: 6px;
-    @media (max-width: 767px) {
-      padding: 0 12px;
-      letter-spacing: 0.5px;
-    }
-    &:hover, &:focus, &:active {
-      outline: none;
-      box-shadow: none;
-    }
+  border: 1px solid var(--newletter-submit-border);
+  font-weight: 720;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  border-radius: 4px;
+  text-decoration: none !important;
+  outline: none;
+  box-shadow: var(--button-shadow);
 
-    &:disabled {
-      cursor: not-allowed;
-      color: var(--icon-color);
-      background: var(--accent-color-1);
-      font-weight: 600;
+  &::after {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    top: 10px;
+    left: 9px;
+    border: 1px solid var(--newsletter-backdrop);
+    z-index: -2;
+    position: absolute;
+    border-radius: 4px;
+    background-color: var(--background-color);
+  }
+
+  .icon-email {
+    height: 14px;
+    width: 14px;
+    fill: transparent;
+    stroke: var(--icon-color);
+    position: relative;
+    top: 3px;
+    margin-right: 4px;
+  }
+
+  &:hover, &:focus, &:active {
+    outline: none;
+    box-shadow: none;
+    background: var(--note-color);
+    color: var(--background-color);
+    transition: all .3s ease;
+    .icon-email {
+      transition: all .3s ease;
+      stroke: var(--line-color);
+    }
+    &::after {
+      transition: all .3s ease;
+      top: 0;
+      left: 0;
     }
   }
 }
