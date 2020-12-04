80931d0a7edc41b58eb972b395ac6e56c6d48ed2
Yusuf Parak <yusuf.parak@gmail.com>
2020-12-05 00:25:16 +0200

    Add default styles

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,7 @@
 </template>
 
 <style lang="scss">
+.content {
+  background-color: var(--note-color);
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,12 @@ module.exports = {
     loaderOptions: {
       sass: {
         prependData: `
+          @import "@/styles/_reset.scss";
           @import "@/styles/_variables.scss";
+          @import "@/styles/_helpers.scss";
+          @import "@/styles/_mixins.scss";
+          @import "@/styles/_typography.scss";
+          @import "@/styles/_main.scss";
         `,
       },
     },
