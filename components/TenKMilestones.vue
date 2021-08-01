a8b3d071acf59318d92325d95d0de2e24de21e06
Yusuf Parak <yusuf@ycparak.com>
2021-08-01 10:43:03 +0200

    Update challenge page

--- components/TenKMilestones.vue
+++ components/TenKMilestones.vue
@@ -4,8 +4,8 @@
     <div class="tenk-widget-content milestones-content">
       <TenKMilestonesItem title="Save 2.5 years runway" date="25 May 2021" :complete="true" />
       <TenKMilestonesItem title="Leave job" date="16 Jul 2021" :complete="true" />
-      <TenKMilestonesItem title="Start challenge" date="01 Aug 2021" :complete="false" />
-      <TenKMilestonesItem title="First sale / bit of revenue" date="Momentous" :complete="false" />
+      <TenKMilestonesItem title="Start challenge" date="01 Aug 2021" :complete="true" />
+      <TenKMilestonesItem title="First sale / bit of revenue" date="Don’t care how" :complete="false" />
       <TenKMilestonesItem title="$1k MRR" date="Ramen profitability" :complete="false" />
       <TenKMilestonesItem title="$4k MRR" date="Replicates old base salary" :complete="false" />
       <TenKMilestonesItem title="$5k MRR" date="Halfway" :complete="false" />
